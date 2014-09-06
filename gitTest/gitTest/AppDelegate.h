8194f2f3d8f91dce81e0d71075faec4d966b96da
zhangfan <649823913@qq.com>
2014-09-06 14:00:49 +0800

    add project file

---
+++ gitTest/gitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  gitTest
+//
+//  Created by Youni.Zhangfan on 9/6/14.
+//  Copyright (c) 2014 Youni.Zhangfan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
